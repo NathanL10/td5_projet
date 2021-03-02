a395dbab0f597daba82ca9807381fc42fff46fee
Nathan <root@9089171X236013.localdomain>
2021-03-02 18:51:55 +0100

    Exo 2 et debut Exo3

--- book.py
+++ book.py
@@ -2,12 +2,22 @@
 
 class Book:
     
-    def __init__(self, nom, order_list = []):
-        self.nom = nom
+    def __init__(self, name, order_list = []):
+        self.name = name
         self.order_list = order_list
+        
+    def insert_buy(quantity, price):
+        return 0
+        
+    def insert_sell(quantity, price):
+        return 0
         
 class Order:
     
-    def __init__(self, nom, order_list = []):
-        self.nom = nom
-        self.order_list = order_list
+    def __init__(self, order_type, quantity, price, idd):
+        self.order_type = order_type
+        self.quantity = quantity
+        self.price = price
+        self.idd = 0
+        
+    
