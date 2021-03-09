bbc435adc03ca1644beae69134b440ca3ae03717
Nathan <root@9089171X236013.localdomain>
2021-03-09 18:38:14 +0100

    Exo 3 fini

--- book.py
+++ book.py
@@ -2,15 +2,101 @@
 
 class Book:
     
-    def __init__(self, name, order_list = []):
+    def __init__(self, name, count = 1, order_list = []):
         self.name = name
+        self.count = count
         self.order_list = order_list
         
-    def insert_buy(quantity, price):
-        return 0
+    def __str__(self):
+        s = "Book on %s \n"%(self.name)
+        for o in self.order_list:
+            s += "         %s %s @ %s id = %s \n"%(o.order_type, o.quantity, o.price, o.idd)
+        s +="------------------------\n"
+        return s
+    
+    def insert_buy(self, quantity, price):
         
-    def insert_sell(quantity, price):
-        return 0
+        o = Order("BUY",quantity,price,self.count)
+        
+        print("--- Insert BUY %s @ %s id = %s on %s"%(quantity,price,self.count,self.name))
+        
+        self.count += 1
+        toRemove=[]
+        
+        if self.order_list == [] :
+            self.order_list.append(o)
+            
+        else :
+            for i in range(len(self.order_list)-1,-1,-1) :
+                if self.order_list[i].order_type == "SELL":
+                    if o.price >= self.order_list[i].price :
+                        if o.quantity > self.order_list[i].quantity :
+                            o.quantity -= self.order_list[i].quantity
+                            toRemove.append(i)
+                            print("Execute %s at %s on %s"%(self.order_list[i].quantity, self.order_list[i].price, self.name))
+                        elif o.quantity == self.order_list[i].quantity :
+                            toRemove.append(i)
+                            print("Execute %s at %s on %s"%(o.quantity, self.order_list[i].price, self.name))
+                            break
+                        else :
+                            self.order_list[i].quantity -= o.quantity
+                            print("Execute %s at %s on %s"%(o.quantity, self.order_list[i].price, self.name))
+                            break   
+                    else :
+                        self.order_list.append(o) 
+                        break
+            
+            for i in toRemove:
+                self.order_list.pop(i)
+            
+            #tri
+            self.order_list = sorted(self.order_list, key=lambda order: order.price, reverse=True)
+        
+        #print
+        print(self)
+        
+    def insert_sell(self, quantity, price):
+        
+        o = Order("SELL",quantity,price,self.count)
+        
+        print("--- Insert SELL %s @ %s id = %s on %s"%(quantity,price,self.count,self.name))
+        
+        self.count += 1
+        toRemove=[]
+        
+        if self.order_list == [] :
+            self.order_list.append(o)
+        
+        else :
+            for i in range(len(self.order_list)) :
+                if self.order_list[i].order_type == "BUY":
+                    if o.price <= self.order_list[i].price :
+                        if o.quantity > self.order_list[i].quantity :
+                            o.quantity -= self.order_list[i].quantity
+                            toRemove.append(i)
+                            print("Execute %s at %s on %s"%(self.order_list[i].quantity, self.order_list[i].price, self.name))
+                        elif o.quantity == self.order_list[i].quantity :
+                            toRemove.append(i)
+                            print("Execute %s at %s on %s"%(o.quantity, self.order_list[i].price, self.name))
+                            break
+                        else :
+                            self.order_list[i].quantity -= o.quantity
+                            print("Execute %s at %s on %s"%(o.quantity, self.order_list[i].price, self.name))
+                            break
+                        
+                    else :
+                        self.order_list.append(o) 
+                        break
+              
+            for i in toRemove:
+                self.order_list.pop(i)
+            
+            #tri
+            self.order_list = sorted(self.order_list, key=lambda order: order.price, reverse=True)
+        
+        #print
+        print(self)
+
         
 class Order:
     
@@ -18,6 +104,6 @@ class Order:
         self.order_type = order_type
         self.quantity = quantity
         self.price = price
-        self.idd = 0
+        self.idd = idd
         
     
